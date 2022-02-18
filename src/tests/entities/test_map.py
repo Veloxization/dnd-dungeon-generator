af77dcfabc89ac044d9005f1cc721aa802c32f51
Veloxization <github@velo.anonaddy.com>
2022-02-18 12:11:08 +0200

    Changed the map tests to reflect renewed function name

--- src/tests/entities/test_map.py
+++ src/tests/entities/test_map.py
@@ -10,13 +10,13 @@ class TestMap(unittest.TestCase):
         self.assertAlmostEqual(self.test_map1.map_height, 20)
 
     def test_the_top_left_corner_only_has_two_neighbours(self):
-        self.assertEqual(len(self.test_map1._get_adjacent_cells(0, 0)), 2)
+        self.assertEqual(len(self.test_map1.get_adjacent_cells(0, 0)), 2)
 
     def test_an_edge_cell_has_three_neighbours(self):
-        self.assertEqual(len(self.test_map1._get_adjacent_cells(0,1)), 3)
+        self.assertEqual(len(self.test_map1.get_adjacent_cells(0,1)), 3)
 
     def test_a_middle_cell_has_four_neighbours(self):
-        self.assertEqual(len(self.test_map1._get_adjacent_cells(1, 1)), 4)
+        self.assertEqual(len(self.test_map1.get_adjacent_cells(1, 1)), 4)
 
     def test_an_occupied_cells_status_changes_correctly(self):
         self.assertNotIn((0,0), self.test_map1.cells)
