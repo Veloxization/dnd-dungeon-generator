67676538790fc4d1ff64eb624b1bd23e653c0bbc
Veloxization <github@velo.anonaddy.com>
2022-02-26 16:54:38 +0200

    Modified image generation service to match the intended vision

--- src/services/image_generation_service.py
+++ src/services/image_generation_service.py
@@ -21,7 +21,8 @@ class ImageGenerationService:
         self._map_width = map_width
         self._map_height = map_height
         self.image = Image.new("RGBA", (map_width, map_height), color=(0,0,0,255))
-        self._image_draw = ImageDraw.Draw(self.image)
+        self.room_image = Image.new("RGBA", (self._map_width, self._map_height), color=(0,0,0,0))
+        self.grid_image = Image.new("RGBA", (self._map_width, self._map_height), color=(0,0,0,0))
 
     def draw_room(self, room_position_x=0, room_position_y=0, room_width=100, room_height=100):
         """Draws a room of specified dimensions and position
@@ -35,9 +36,10 @@ class ImageGenerationService:
             room_height: The height of the room in pixels
         """
 
+        image_draw = ImageDraw.Draw(self.room_image)
         lower_right_x = room_position_x + room_width
         lower_right_y = room_position_y + room_height
-        self._image_draw.rectangle((room_position_x, room_position_y, lower_right_x, lower_right_y),
+        image_draw.rectangle((room_position_x, room_position_y, lower_right_x, lower_right_y),
                                     fill=(255,255,255,128))
 
     def draw_grid(self, cell_width=20, cell_height=20, line_thickness=1):
@@ -48,20 +50,23 @@ class ImageGenerationService:
             cell_height: The height of an individual grid cell in pixels
             line_thickness: The thickness of the grid lines in pixels
         """
+
+        image_draw = ImageDraw.Draw(self.grid_image)
         line_color = (128,128,128,64)
         current_coordinate = 0
         while current_coordinate < self._map_width:
-            self._image_draw.line((current_coordinate, 0, current_coordinate, self._map_height),
-                                   fill=line_color,
-                                   width=line_thickness)
+            image_draw.line((current_coordinate, 0, current_coordinate, self._map_height),
+                             fill=line_color,
+                             width=line_thickness)
             current_coordinate += cell_width
 
         current_coordinate = 0
         while current_coordinate < self._map_height:
-            self._image_draw.line((0, current_coordinate, self._map_width, current_coordinate),
-                                   fill=line_color,
-                                   width=line_thickness)
+            image_draw.line((0, current_coordinate, self._map_width, current_coordinate),
+                             fill=line_color,
+                             width=line_thickness)
             current_coordinate += cell_height
+        self.image.alpha_composite(self.grid_image)
 
     def get_generated_image(self):
         """Get the generated image
@@ -70,4 +75,6 @@ class ImageGenerationService:
             The generated Image object in whatever form it is in
         """
 
+        self.image.alpha_composite(self.room_image)
+        self.image.alpha_composite(self.grid_image)
         return self.image
